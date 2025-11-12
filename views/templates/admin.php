365b2e40c5885a9eec1517dabf5b80e46029719d
Christelle Giga <christellebento@hotmail.com>
2025-11-12 11:26:25 +0100

    implementation css fix table

--- views/templates/admin.php
+++ views/templates/admin.php
@@ -11,27 +11,57 @@
 <h2>Edition des articles</h2>
 
 <div class="adminArticle">
-        <div class="articleLine header">
-            <div class="title <?= $sortField === 'title' ? 'active' : ''; ?>"><a href="index.php?action=admin&sortField=title&sortOrder=<?= $sortOrder ?>"><?= $sortOrder === 'asc' ? '⬆' : '⬇'; ?></a></div>
-            <div class="content <?= $sortField === 'content' ? 'active' : ''; ?>"><a href="index.php?action=admin&sortField=content&sortOrder=<?= $sortOrder ?>"><?= $sortOrder === 'asc' ? '⬆' : '⬇'; ?></a></div>
-            <div class="content <?= $sortField === 'views' ? 'active' : ''; ?>"><a href="index.php?action=admin&sortField=views&sortOrder=<?= $sortOrder ?>"><?= $sortOrder === 'asc' ? '⬆' : '⬇'; ?></a></div>
-            <div class="content <?= $sortField === 'date_creation' ? 'active' : ''; ?>"><a href="index.php?action=admin&sortField=date_creation&sortOrder=<?= $sortOrder ?>"><?= $sortOrder === 'asc' ? '⬆' : '⬇'; ?></a></div>
-            <div class="content <?= $sortField === 'commentCount' ? 'active' : ''; ?>"><a href="index.php?action=admin&sortField=commentCount&sortOrder=<?= $sortOrder ?>"><?= $sortOrder === 'asc' ? '⬆' : '⬇'; ?></a></div>
-            <div></div>
-            <div></div>
-        </div>
-    <?php foreach ($articles as $article) { ?>
-        <div class="articleLine">
-            <div class="title"><?= $article->getTitle() ?></div>
-            <div class="content"><?= $article->getContent(200) ?></div>
-            <div class="content"><?= $article->getViews()?></div>
-            <div class="content"><?= $article->getDateCreation()->format('d/m/Y') ?></div>
-            <div class="content"><?= $article->getCommentCount() ?></div>
-
-            <div><a class="submit" href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Modifier</a></div>
-            <div><a class="submit" href="index.php?action=deleteArticle&id=<?= $article->getId() ?>" <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer cet article ?") ?> >Supprimer</a></div>
-        </div>
-    <?php } ?>
+    <table class="articleTable">
+        <thead>
+            <tr>
+                <th class="<?= $sortField === 'title' ? 'active' : ''; ?>">
+                    Titre
+                    <a href="index.php?action=admin&sortField=title&sortOrder=<?= $sortOrder ?>">
+                        <?= $sortOrder === 'asc' ? '⬆' : '⬇'; ?>
+                    </a>
+                </th>
+                <th class="<?= $sortField === 'content' ? 'active' : ''; ?>">
+                    Article
+                    <a href="index.php?action=admin&sortField=content&sortOrder=<?= $sortOrder ?>">
+                        <?= $sortOrder === 'asc' ? '⬆' : '⬇'; ?>
+                    </a>
+                </th>
+                <th class="<?= $sortField === 'views' ? 'active' : ''; ?>">
+                    Vues
+                    <a href="index.php?action=admin&sortField=views&sortOrder=<?= $sortOrder ?>">
+                        <?= $sortOrder === 'asc' ? '⬆' : '⬇'; ?>
+                    </a>
+                </th>
+                <th class="<?= $sortField === 'date_creation' ? 'active' : ''; ?>">
+                    Date
+                    <a href="index.php?action=admin&sortField=date_creation&sortOrder=<?= $sortOrder ?>">
+                        <?= $sortOrder === 'asc' ? '⬆' : '⬇'; ?>
+                    </a>
+                </th>
+                <th class="<?= $sortField === 'commentCount' ? 'active' : ''; ?>">
+                    Nb. comm.
+                    <a href="index.php?action=admin&sortField=commentCount&sortOrder=<?= $sortOrder ?>">
+                        <?= $sortOrder === 'asc' ? '⬆' : '⬇'; ?>
+                    </a>
+                </th>
+                <th colspan="2">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($articles as $article) { ?>
+                <tr>
+                    <td class="title"><?= $article->getTitle() ?></td>
+                    <td class="content"><?= $article->getContent(200) ?></td>
+                    <td class="content"><?= $article->getViews() ?></td>
+                    <td class="content"><?= $article->getDateCreation()->format('d/m/Y') ?></td>
+                    <td class="content"><?= $article->getCommentCount() ?></td>
+                    <td><a class="submit" href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Modifier</a></td>
+                    <td><a class="submit" href="index.php?action=deleteArticle&id=<?= $article->getId() ?>" <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer cet article ?") ?>>Supprimer</a></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 </div>
+
 
 <a class="submit" href="index.php?action=showUpdateArticleForm">Ajouter un article</a>
